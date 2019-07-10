7b357544597a0059faccff8be5242c204d595204
Michele Mazzamuto <michele.mazzamuto95@yahoo.com>
2019-07-11 00:41:13 +0200

    add webcam style

--- src/actions/index.js
+++ src/actions/index.js
@@ -37,3 +37,36 @@ export const loginWithToken = () => (dispatch, getState) => {
     dispatch,
   })
 }
+
+
+export const sendPicture = foto => dispatch => {
+  console.log('Upload')
+  var data = { 'data': foto }
+  fetch('http://localhost:5000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      //Authorization: `Bearer ${token}`,
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(data),
+    mode: 'cors',
+  })
+    //.then((response) => status = response.status)
+    .then(response => response.json())
+
+    .then(response =>
+      
+      dispatch({
+        type: 'SET_CLASS',
+        payload: response,
+      }),
+    )
+    .catch(err => {
+      dispatch({
+        //type: 'SET_ERROR',
+        //payload: err,
+      })
+    })
+
+}
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,12 +1,24 @@
 import { actionTypes as types } from '../constants'
 
-const user = (state = {}, action) => {
+const INITIAL_STATE = {
+  token: [],
+  class: 0,
+  accuracy: 0,
+}
+const user = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case types.SIGNUP_SUCCESS:
     case types.LOGIN_SUCCESS:
       return action.data
-    case types.LOGIN_FAILURE:
-      return {}
+    case 'SET_CLASS':
+      console.log('diooo')
+      console.log(action.payload)
+      return {
+        ...state,
+
+        class: action.payload.Class,
+        accuracy: action.payload.Accuracy.toFixed(2)
+      }
     default:
       return state
   }
